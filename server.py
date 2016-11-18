7df40260f75edd6314a4418acf74948c01b1a259
Alex Bannerjee <alexbannerjee@gmail.com>
2016-11-17 23:59:37 -0500

    Refactor server.py by creating an Application class to hold the tornado web app. In addition, handlers for specific urls have been moved into server.py to allow a shared DB connection object to be accessible across all handlers.

--- server.py
+++ server.py
@@ -1,16 +1,43 @@
+#!/usr/bin/env python
+
 """Main API Server
 
 Provides the REST API for the Widget Factory app.
 """
 from tornado.ioloop import IOLoop
-from tornado.web import Application
-from urls import urls
+import tornado.web
+import psycopg2
+import logging
 
-def make_app():
-    return Application(urls)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        # PostgreSQL DB Connection shared across all handlers
+        try:
+            conn = psycopg2.connect("dbname='widgetdb' user='widgetapi' host='localhost'")
+            self.db = conn
+        except:
+            logging.error('Error: Unable to connect to the database')
+
+        urls = [
+            (r"/widget/([0-9]+)", WidgetHandler),
+        ]
+
+        super(Application, self).__init__(urls)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+class WidgetHandler(BaseHandler):
+    def get(self, id):
+        self.write("GET on widget: {}".format(id))
+
 
 def main():
-    app = make_app()
+    app = Application()
     app.listen(3000)
     IOLoop.current().start()
 
--- test/widget.py
+++ test/widget.py
@@ -1,14 +1,13 @@
 from tornado.testing import AsyncHTTPTestCase
 from tornado.web import Application
 from tornado.httpserver import HTTPRequest
-from routes.widget import Handler
 import server
 import unittest
 
 class TestWidgetHandler(AsyncHTTPTestCase):
 
     def get_app(self):
-        return server.make_app()
+        return server.Application()
 
     def test_widget_get(self):
         response = self.fetch('/widget/2')
