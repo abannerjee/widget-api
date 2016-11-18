675217f34ec158d1771bff5b25eda4a6d94c3c51
Alex Bannerjee <alexbannerjee@gmail.com>
2016-11-18 00:00:54 -0500

    Add a sql script to initialize the postgresql db.

---
+++ db/init.sql
@@ -0,0 +1,44 @@
+-- Clear Existing Schema
+DROP SCHEMA IF EXISTS widget CASCADE;
+CREATE SCHEMA widget AUTHORIZATION widgetapi;
+
+-- Grant permissions
+GRANT ALL privileges ON ALL TABLES IN SCHEMA widget TO widgetapi;
+GRANT USAGE ON SCHEMA widget TO widgetapi;
+
+-- Users
+CREATE TABLE widget.user (
+  id SERIAL PRIMARY KEY,
+  name varchar NOT NULL,
+  pw varchar NOT NULL
+);
+
+-- Widgets
+CREATE TABLE widget.widget (
+  id SERIAL PRIMARY KEY,
+  name varchar NOT NULL
+);
+
+-- Properties
+CREATE TABLE widget.property (
+  id SERIAL PRIMARY KEY,
+  type varchar NOT NULL,
+  display_name varchar NOT NULL,
+  value varchar
+);
+
+-- WidgetProperty
+CREATE TABLE widget.widget_property (
+  id SERIAL PRIMARY KEY,
+  widget_id integer REFERENCES widget.widget (id),
+  property_id integer REFERENCES widget.property (id)
+);
+
+-- Orders
+CREATE TABLE widget.order (
+  id SERIAL PRIMARY KEY,
+  created_on timestamp default current_timestamp,
+  user_id integer REFERENCES widget.user (id),
+  widget_id integer REFERENCES widget.widget (id),
+  quantity integer NOT NULL
+);
