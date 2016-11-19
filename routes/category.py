cd4a962ed59272a67de97db49548f013fed1b6ed
Alex Bannerjee <alexbannerjee@gmail.com>
2016-11-18 19:48:34 -0500

    Update category endpoint unit test and route handler.

--- routes/category.py
+++ routes/category.py
@@ -1,16 +1,17 @@
-""" Category Handler
+"""Category Handler
 
 Supports [ GET ]
 
-GET - Returns all categories in the DB
-      A category is defined as a property with
-      'category' == 'type'
 """
 
 from . import common as c
 
 class Handler(c.BaseHandler):
+
+    """ Returns all categories in the DB. A category is defined
+    as a property with 'category' == 'type'."""
     def get(self):
+        query = "SELECT * FROM {}.property WHERE p_category = 'type'".format(self.schema)
         cur = self.db.cursor()
-        cur.execute("SELECT * FROM widget.property WHERE category = 'type'")
+        cur.execute(query)
         self.write(c.formatdata(cur))
---
+++ test/category.py
@@ -0,0 +1,21 @@
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import Application
+from tornado.httpserver import HTTPRequest
+from test.config import config
+import server
+import unittest
+import json
+
+class TestCategoryHandler(AsyncHTTPTestCase):
+
+    def get_app(self):
+        return server.Application(config)
+
+    def test_get(self):
+        response = self.fetch('/categories')
+        res = json.loads(response.body.decode('utf-8'))
+        self.assertEqual(response.code, 200)
+        self.assertEqual(len(res), 1)
+
+if __name__ == '__main__':
+    unittest.main()
