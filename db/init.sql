48f39796a539c4cc32969c23425fcf51de0bafa3
Alex Bannerjee <alexbannerjee@gmail.com>
2016-11-20 11:10:19 -0500

    Update DB schema to allow widgets to be hierarchical instead of properties. Add a column of integer arrays to the order table to store information on how a widget is configured.

--- db/init.sql
+++ db/init.sql
@@ -12,27 +12,27 @@ GRANT USAGE ON SCHEMA test TO widgetapi;
 
 -- MAIN SCHEMA (widget)
 
+-- Widgets
+CREATE TABLE widget.widget (
+  w_id SERIAL PRIMARY KEY,
+  w_name varchar NOT NULL,
+  w_parent integer REFERENCES widget.widget,
+  w_depth smallint DEFAULT 0
+);
+
 -- Inventory
 CREATE TABLE widget.inventory (
   i_id SERIAL PRIMARY KEY,
   i_widget_id integer REFERENCES widget.widget,
   i_stock integer DEFAULT 0
-)
-
--- Widgets
-CREATE TABLE widget.widget (
-  w_id SERIAL PRIMARY KEY,
-  w_name varchar NOT NULL
 );
 
 -- Properties
 CREATE TABLE widget.property (
   p_id SERIAL PRIMARY KEY,
-  p_category varchar NOT NULL,
-  p_display_name varchar NOT NULL,
-  p_value varchar,
-  p_parent integer REFERENCES widget.property,
-  p_depth smallint DEFAULT 0
+  p_type varchar NOT NULL,
+  p_name varchar NOT NULL,
+  p_value varchar
 );
 
 -- WidgetProperty
@@ -42,38 +42,39 @@ CREATE TABLE widget.widget_property (
   wp_property_id integer REFERENCES widget.property
 );
 
+
 -- Orders
 CREATE TABLE widget.order (
   o_id SERIAL PRIMARY KEY,
   o_created_on timestamp default current_timestamp,
   o_widget_id integer REFERENCES widget.widget,
-  o_quantity integer NOT NULL
+  o_configuration integer[]
 );
 
 
 -- TEST SCHEMA (test)
 
--- Inventory
-CREATE TABLE test.inventory (
-  i_id SERIAL PRIMARY KEY,
-  i_widget_id integer REFERENCES widget.widget,
-  i_stock integer DEFAULT 0
-)
-
 -- Widgets
 CREATE TABLE test.widget (
   w_id SERIAL PRIMARY KEY,
-  w_name varchar NOT NULL
+  w_name varchar NOT NULL,
+  w_parent integer REFERENCES test.widget,
+  w_depth smallint DEFAULT 0
+);
+
+-- Inventory
+CREATE TABLE test.inventory (
+  i_id SERIAL PRIMARY KEY,
+  i_widget_id integer REFERENCES test.widget,
+  i_stock integer DEFAULT 0
 );
 
 -- Properties
 CREATE TABLE test.property (
   p_id SERIAL PRIMARY KEY,
-  p_category varchar NOT NULL,
-  p_display_name varchar NOT NULL,
-  p_value varchar,
-  p_parent integer REFERENCES test.property,
-  p_depth smallint DEFAULT 0
+  p_type varchar NOT NULL,
+  p_name varchar NOT NULL,
+  p_value varchar
 );
 
 -- WidgetProperty
@@ -88,5 +89,5 @@ CREATE TABLE test.order (
   o_id SERIAL PRIMARY KEY,
   o_created_on timestamp default current_timestamp,
   o_widget_id integer REFERENCES test.widget,
-  o_quantity integer NOT NULL
+  o_configuration integer[]
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,36 +1,33 @@
 -- MAIN SCHEMA (widget)
 
-INSERT INTO widget.property (p_category, p_display_name) VALUES
-  ('type', 'prime'),
-  ('type', 'elite'),
-  ('type', 'extreme edition'),
-  ('color', 'red'),
-  ('color', 'white'),
-  ('color', 'blue'),
-  ('color', 'chrome');
+INSERT INTO widget.widget (w_name, w_parent, w_depth) VALUES
+  ('prime', NULL, 0),
+  ('elite', NULL, 0),
+  ('extreme edition', NULL, 0);
 
-INSERT INTO widget.property (p_category, p_display_name, p_value) VALUES
-  ('size', 'small', '1'),
-  ('size', 'medium', '5'),
-  ('size', 'large', '10'),
-  ('size', 'x-large', '12');
+INSERT INTO widget.property (p_type, p_name, p_value) VALUES
+  ('color', 'red', NULL),
+  ('color', 'white', NULL),
+  ('color', 'blue', NULL),
+  ('color', 'chrome', NULL),
+  ('size', 'invisibly small', '1'),
+  ('size', 'slightly medium', '5'),
+  ('size', 'somewhat large', '10'),
+  ('size', 'galactically huge', '100');
 
 -- TEST SCHEMA (test)
 
-INSERT INTO test.property (p_category, p_display_name, p_value)
-VALUES ('type', 'test-type', '1');
+INSERT INTO test.widget (w_name, w_parent, w_depth) VALUES
+  ('test-widget', NULL, 0);
 
-INSERT INTO test.property (p_category, p_display_name, p_value, p_parent, p_depth)
-VALUES ('sub-type', 'prime-sub', '2', '1', '1');
+INSERT INTO test.property (p_type, p_name, p_value) VALUES
+  ('test-type', 'test-property', '1');
 
-INSERT INTO test.widget (w_name)
-VALUES ('test-widget');
+INSERT INTO test.inventory (i_widget_id, i_stock) VALUES
+  (1, 100);
 
-INSERT INTO test.inventory (i_widget_id, i_stock)
-VALUES (1, 100);
+INSERT INTO test.widget_property (wp_widget_id, wp_property_id) VALUES
+  (1, 1);
 
-INSERT INTO test.widget_property (wp_widget_id, wp_property_id)
-VALUES (1, 1);
-
-INSERT INTO test.order (o_widget_id, o_quantity)
-VALUES (1, 10);
+INSERT INTO test.order (o_widget_id, o_configuration) VALUES
+  (1, [1,5]);
